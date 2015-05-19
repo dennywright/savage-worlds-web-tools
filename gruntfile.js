4bb9e9eb1ca1df47a46f7a05f44d48bbbb6dd2da
Denny Wright <dennyw17@gmail.com>
2015-05-19 17:22:46 -0600

    Fix grunt to build, serve and watch for changes
    Added grunt-jekyll to build the web page
    Added grunt-contrib-connect to serve up the web page

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
+
 		less: {
 			development: {
 				options: {
@@ -21,10 +22,11 @@ module.exports = function(grunt) {
 					sourceMapName: 'js/sourcemap.map'
 				},
 				files: {
-					'js/swwt.min.js' : ['js/src/*.js', 'js/src/chargen/*.js', 'js/src/extras/*.js', 'js/src/creators/*.js'],
+					'js/swwt.min.js' : ['js/src/*.js', 'js/src/chargen/*.js', 'js/src/extras/*.js', 'js/src/creators/*.js']
 				}
 			}
 		},
+
 		watch: {
 			styles: {
 				// Which files to watch (all .less files recursively in the less directory)
@@ -42,12 +44,32 @@ module.exports = function(grunt) {
 					nospawn: true
 				}
 			}
+		},
+
+		connect: {
+			default: {
+				options: {
+					hostname: 'localhost',
+					port: 9000,
+					livereload: true,
+					base: './_site',
+					open: { appName: 'chrome' }
+				}
+			}
+		},
+
+		jekyll: {
+			default: {
+			}
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-jekyll');
 
-	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['jekyll','connect','watch']);
 };
